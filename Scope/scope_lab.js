82caf7b9ff9c787ad8aff1d00af3ef0bf387d3bf
Cristian <cristian.triboi@gmail.com>
2024-10-09 15:15:27 -0400

    Test some block and some global vars

---
+++ Scope/scope_lab.js
@@ -0,0 +1,26 @@
+// Global scope
+var globalVar = "I'm a global variable";
+let globalLet = "I'm also global, but scoped with let";
+const globalConst = "I'm a global constant";
+
+
+// Block scope
+function show() {
+var blockVar = "I'm a block-scoped var";
+let blockLet = "I'm a block-scoped let";
+const blockConst = "I'm a block-scoped const";
+}
+show()
+
+// Global scope
+console.log(globalVar); // Output: "I'm a global variable"
+console.log(globalLet); // Output: "I'm also global, but scoped with let"
+console.log(globalConst); // Output: "I'm a global constant"
+
+//Block Scope
+//console.log(blockVar);
+//console.log(blockLet);
+
+console.log(functionVar); // Throws ReferenceError
+console.log(functionLet); // Throws ReferenceError
+console.log(functionConst); // Throws ReferenceError
